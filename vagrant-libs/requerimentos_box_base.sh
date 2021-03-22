a0b2550549ca4856be85bfc1e5b22d7a316674dc
neoricalex <neo.webmaster.2@gmail.com>
2021-03-22 18:53:53 -0300

    Atualização Automática via bash shell

--- vagrant-libs/requerimentos_box_base.sh
+++ vagrant-libs/requerimentos_box_base.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-echo "==> Criar a pasta compartilhada ..."
-mkdir -p /home/vagrant/src
-mount.vboxsf -o "uid=1000,gid=1000,dev,exec,rw" pasta_compartilhada /home/vagrant/src
+#echo "==> Criar a pasta compartilhada ..."
+#mkdir -p /home/vagrant/src
+#mount.vboxsf -o "uid=1000,gid=1000,dev,exec,rw" pasta_compartilhada /home/vagrant/src
 
 echo "==> Atualizar os repositórios do $HOSTNAME ..."
 sudo apt update && sudo apt upgrade -y
