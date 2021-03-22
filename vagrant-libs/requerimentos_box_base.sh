600e2abceeb6814d148fe99938f94791a1472bc8
neoricalex <neo.webmaster.2@gmail.com>
2021-03-22 14:51:05 -0300

    Atualização Automática via bash shell

---
+++ vagrant-libs/requerimentos_box_base.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+echo "==> Atualizar os repositórios do $HOSTNAME ..."
+sudo apt update
+
+echo "==> Instalar os pacotes base no $HOSTNAME..."
+sudo apt-get install -y \
+    linux-generic \
+    linux-headers-`uname -r` \
+    ubuntu-minimal \
+    dkms \
+    autoconf \
+    build-essential \
+    make 
