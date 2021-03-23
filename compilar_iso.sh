6864bee633214d47b99bf8288708a8346043a381
neoricalex <neo.webmaster.2@gmail.com>
2021-03-23 09:54:43 -0300

    Atualização Automática via bash shell

--- compilar_iso.sh
+++ compilar_iso.sh
@@ -58,7 +58,7 @@ if [ ! -f "$NFDOS_HOME/desktop/vagrant/NFDOS-$NFDOS_VERSAO.box" ]; then
 
     echo "Checkando o SHA256 da imagem ISO..."
     checkar_sha256=$(sha256sum $NFDOS_ROOT/nfdos.iso | awk '{ print $1 }')
-    jq ".variables.iso_checksum = \"$checkar_sha256\"" $NFDOS_HOME/desktop/desktop.json | sponge $NFDOS_HOME/desktop/desktop.json
+    jq ".variables.iso_checksum = \"$checkar_sha256\"" $NFDOS_HOME/desktop/libvirt.json | sponge $NFDOS_HOME/desktop/libvirt.json
 
     cd $NFDOS_HOME/desktop
     packer build desktop.json #VBoxManage setextradata VM-name "VBoxInternal/TM/TSCTiedToExecution" 1
