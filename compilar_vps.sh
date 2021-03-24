99ac008975f4d27b3b18645aee43c4caf2ce0896
neoricalex <neo.webmaster.2@gmail.com>
2021-03-24 05:11:22 -0300

    Atualização Automática via bash shell

--- compilar_vps.sh
+++ compilar_vps.sh
@@ -159,7 +159,14 @@ VAGRANT_VAGRANTFILE=Vagrantfile.VPS_DEV vagrant ssh<<EOF
 
 cd /vagrant
 make iso
-vboxmanage list vms
+
+echo "==> Download Vagrant & Instalar"
+wget -nv https://releases.hashicorp.com/vagrant/2.2.9/vagrant_2.2.9_x86_64.deb
+dpkg -i vagrant_2.2.9_x86_64.deb
+rm vagrant_2.2.9_x86_64.deb
+
+echo "==> Instalar plugins do Vagrant"
+vagrant plugin install vagrant-libvirt
 
 cd .. 
 EOF
