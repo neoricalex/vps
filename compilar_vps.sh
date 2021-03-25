05494627e3c551bcd91cd97785710bbb18452b43
neoricalex <neo.webmaster.2@gmail.com>
2021-03-24 22:05:52 -0300

    Atualizar para reduzir o tamanho

--- compilar_vps.sh
+++ compilar_vps.sh
@@ -1,90 +1,22 @@
 #!/bin/bash
 
-instalar_virtualbox(){
-    echo "==> Instalar o VirtualBox"
-    sudo apt install -y virtualbox
-    sudo apt install -y virtualbox-guest-dkms virtualbox-guest-x11
-    sudo apt install -y virtualbox-guest-additions-iso
-
-    echo "==> Instalar o Extension Pack do VirtualBox"
-    wget https://download.virtualbox.org/virtualbox/6.1.18/Oracle_VM_VirtualBox_Extension_Pack-6.1.18.vbox-extpack
-    sudo vboxmanage extpack install Oracle_VM_VirtualBox_Extension_Pack-6.1.18.vbox-extpack --accept-license=33d7284dc4a0ece381196fda3cfe2ed0e1e8e7ed7f27b9a9ebc4ee22e24bd23c
-    rm Oracle_VM_VirtualBox_Extension_Pack-6.1.18.vbox-extpack 
-}
-instalar_vagrant(){
-    echo "==> Download Vagrant & Instalar"
-    wget -nv https://releases.hashicorp.com/vagrant/2.2.9/vagrant_2.2.9_x86_64.deb
-    sudo dpkg -i vagrant_2.2.9_x86_64.deb
-    rm vagrant_2.2.9_x86_64.deb
-
-    echo "==> Instalar plugins do Vagrant"
-    vagrant plugin install vagrant-libvirt
-    vagrant plugin install vagrant-disksize # Só funciona no Virtualbox
-    vagrant plugin install vagrant-mutate
-}
 instalar_requerimentos_para_rodar_vps(){
 	echo "==> Instalar os requerimentos para rodar o VPS_DEV..."
 	if [ ! -f ".requerimentos_host.box" ]; 
 	then
-		echo "==> Atualizar os repositórios..."
-		sudo apt update
-
-		echo "==> Instalar o Linux/Ubuntu base..."
-		sudo apt-get install linux-generic linux-headers-`uname -r` ubuntu-minimal dkms -y
-
-		echo "==> Instalar libvrt & KVM" 
-		# REF: https://github.com/alvistack/ansible-role-virtualbox/blob/master/.travis.yml
-		sudo apt install -y bridge-utils dnsmasq-base ebtables libvirt-daemon-system libvirt-clients \
-			libvirt-dev qemu-kvm qemu-utils qemu-user-static ruby-dev \
-			ruby-libvirt libxslt-dev libxml2-dev zlib1g-dev
-
 		if ! command -v vboxmanage &> /dev/null;
 		then
-			instalar_virtualbox
-		else
-			sudo apt purge virtualbox* -y
-			sudo mv /usr/lib/virtualbox /usr/lib/virtualbox.old
-			sudo apt autoremove -y
-			sleep 1
 			instalar_virtualbox
 		fi
 
 		if ! command -v vagrant &> /dev/null;
 		then
 			instalar_vagrant
-		else
-			sudo apt purge vagrant* -y
-			sudo apt autoremove -y
-			sleep 1
-			instalar_vagrant
 		fi
 
 		echo "==> Removendo pacotes do Ubuntu desnecessários"
 		sudo apt autoremove -y
 		touch .requerimentos_host.box
-
-		#echo "==> Checkando se a box neoricalex/ubuntu existe localmente no $HOSTNAME ..."
-		#if ! vagrant box list | grep "neoricalex/ubuntu" > /dev/null; 
-		#then
-		#	echo "==> Checkando se o download da box já foi feito..."
-		#	if [ ! -f "vagrant-libs/virtualbox.box" ]; 
-		#	then
-		#		echo "Iniciando o download..."
-		#		cd vagrant-libs
-		#		wget https://vagrantcloud.com/ubuntu/boxes/focal64/versions/20210320.0.0/providers/virtualbox.box \
-		#			-q --show-progress \
-		#			--progress=bar:force:noscroll
-		#		cd ..
-		#	fi
-		#	echo "==> O download da box já foi feito."
-
-			#vagrant box add --provider "virtualbox" \
-			#	--box-version "0.0.1" \
-			#	--name "neoricalex/ubuntu" \
-			#	vagrant-libs/virtualbox.box
-
-		#fi
-		#echo "==> A box existe no $HOSTNAME"
 
 	fi
 	echo "==> Os requerimentos para rodar o VPS_DEV foram instalados."
@@ -95,14 +27,14 @@ provisionar_vps(){
 	vps_dev=$(vagrant box list | grep "neoricalex/ubuntu" > /dev/null)
 	if [ $? == "1" ];
 	then
-		echo "==> Checkar se a vagrant-libs/vps_dev.box foi gerada..."
-		if [ ! -f "vagrant-libs/vps_dev.box" ];
+		echo "==> Checkar se a base.box foi gerada..."
+		if [ ! -f "vagrant-libs/base.box" ];
 		then
-			echo "==> Provisionando a vagrant-libs/vps_dev.box..."
+			echo "==> Provisionando a vagrant-libs/base.box..."
 			VAGRANT_VAGRANTFILE=Vagrantfile.Ubuntu vagrant up
-			echo "==> Reiniciando a vagrant-libs/vps_dev.box para as configurações ficarem ativas..."
+			echo "==> Reiniciando a vagrant-libs/base.box para as configurações ficarem ativas..."
 			VAGRANT_VAGRANTFILE=Vagrantfile.Ubuntu vagrant reload
-			echo "==> Entrando na vagrant-libs/vps_dev.box..."
+			echo "==> Entrando na vagrant-libs/base.box..."
 			VAGRANT_VAGRANTFILE=Vagrantfile.Ubuntu vagrant ssh<<EOF
 #!/bin/bash
 
@@ -114,23 +46,23 @@ find ~/.ssh -type f -exec chmod 0600 {} \;
 echo "Limpando..."
 sudo apt-get clean -y 
 EOF
-			echo "==> Empacotando a vagrant-libs/vps_dev.box..."
-			vagrant package --base VPS_DEV --output vagrant-libs/vps_dev.box
+			echo "==> Empacotando a vagrant-libs/base.box..."
+			vagrant package --base VPS_DEV --output vagrant-libs/base.box
 
 			usuario="$(whoami)@$(hostname | cut -d . -f 1-2)"
 			if [ "$usuario" == "neo@desktop" ]; 
 			then
 				vagrant cloud auth login
 				vagrant cloud publish \
-					--box-version 0.0.2 \
+					--box-version 0.0.3 \
 					--release \
 					--short-description "Um VPS baseado no ubuntu/focal64 para desenvolvimento do projeto NEORICALEX e NFDOS" \
-					--version-description "Inserir a Chave SSH Pública" \
-					neoricalex/ubuntu 0.0.2 virtualbox \
-					vagrant-libs/vps_dev.box # --force --debug
+					--version-description "Reduzir o tamanho" \
+					neoricalex/ubuntu 0.0.3 virtualbox \
+					vagrant-libs/base.box # --force --debug
 				vagrant cloud auth logout
 			else
-				echo "[DEBUG] Para enviar a vagrant-libs/vps_dev.box para a Vagrant Cloud tem que ter as credenciais. Continuando..."
+				echo "[DEBUG] Para enviar a vagrant-libs/base.box para a Vagrant Cloud tem que ter as credenciais. Continuando..."
 			fi
 
 			echo "==> Excluir o VPS_DEV baseado no ubuntu/focal64 pois não é mais necessário..."
@@ -139,7 +71,7 @@ EOF
 
 
 		fi
-		echo "==> A vagrant-libs/vps_dev.box foi gerada."
+		echo "==> A vagrant-libs/base.box foi gerada."
 		echo "==> O VPS_DEV baseado no neoricalex/ubuntu está pronto para ser executado."
 		echo "==> Provisionando o neoricalex/ubuntu (VPS_DEV)..."
 		VAGRANT_VAGRANTFILE=Vagrantfile.VPS_DEV vagrant up
--- vagrant-libs/bootstrap.sh
+++ vagrant-libs/bootstrap.sh
@@ -1,10 +1,18 @@
 #!/usr/bin/env bash
 set -eux
 
-# Update Packages
+echo "==> Atualizar os repositórios..."
 sudo apt-get update
 # sudo apt-get -y upgrade
 # sudo apt-get -y dist-upgrade
+
+# Remover entradas antigas do kernel na Grub
+# REF: https://askubuntu.com/questions/176322/removing-old-kernel-entries-in-grub
+sudo apt-get purge $( dpkg --list | grep -P -o "linux-image-\d\S+" | grep -v $(uname -r | grep -P -o ".+\d") ) -y
+
+echo "==> Instalar o Linux/Ubuntu base..."
+sudo apt-get install linux-generic linux-headers-`uname -r` ubuntu-minimal dkms -y
+
 
 # Install Packages
 sudo apt-get install -y build-essential checkinstall libreadline-gplv2-dev \
@@ -12,11 +20,38 @@ sudo apt-get install -y build-essential checkinstall libreadline-gplv2-dev \
     libbz2-dev libffi-dev python3-pip unzip lsb-release software-properties-common \
     curl wget git rsync # python-dev python3-venv
 
-echo "==> Instalar libvrt & KVM"
+echo "==> Instalar o VirtualBox"
+#sudo apt install -y virtualbox
+sudo deb [arch=amd64] https://download.virtualbox.org/virtualbox/debian focal contrib
+wget -q https://www.virtualbox.org/download/oracle_vbox_2016.asc -O- | sudo apt-key add -
+wget -q https://www.virtualbox.org/download/oracle_vbox.asc -O- | sudo apt-key add -
+sudo apt-get update
+sudo apt-get install virtualbox-6.1 -y
+sudo apt install -y virtualbox-guest-dkms virtualbox-guest-x11
+sudo apt install -y virtualbox-guest-additions-iso
+
+echo "==> Instalar o Extension Pack do VirtualBox"
+wget https://download.virtualbox.org/virtualbox/6.1.18/Oracle_VM_VirtualBox_Extension_Pack-6.1.18.vbox-extpack \
+	-q --show-progress \
+	--progress=bar:force:noscroll
+sudo vboxmanage extpack install Oracle_VM_VirtualBox_Extension_Pack-6.1.18.vbox-extpack --accept-license=33d7284dc4a0ece381196fda3cfe2ed0e1e8e7ed7f27b9a9ebc4ee22e24bd23c
+rm Oracle_VM_VirtualBox_Extension_Pack-6.1.18.vbox-extpack 
+
+echo "==> Download Vagrant & Instalar"
+wget -nv https://releases.hashicorp.com/vagrant/2.2.9/vagrant_2.2.9_x86_64.deb
+sudo dpkg -i vagrant_2.2.9_x86_64.deb
+rm vagrant_2.2.9_x86_64.deb
+
+echo "==> Instalar plugins do Vagrant"
+vagrant plugin install vagrant-libvirt
+vagrant plugin install vagrant-disksize # Só funciona no Virtualbox
+vagrant plugin install vagrant-mutate
+
+echo "==> Instalar libvrt & KVM" 
 # REF: https://github.com/alvistack/ansible-role-virtualbox/blob/master/.travis.yml
 sudo apt install -y bridge-utils dnsmasq-base ebtables libvirt-daemon-system libvirt-clients \
-    libvirt-dev qemu-kvm qemu-utils ruby-dev \
-    ruby-libvirt libxslt-dev libxml2-dev zlib1g-dev
+	libvirt-dev qemu-kvm qemu-utils qemu-user-static ruby-dev \
+	ruby-libvirt libxslt-dev libxml2-dev zlib1g-dev
 
 # Install Docker
 sudo curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
@@ -35,5 +70,9 @@ service docker restart
 # Add neo user to docker group
 sudo usermod -aG docker neo
 
+echo "==> Removendo pacotes do Ubuntu desnecessários"
+sudo apt autoremove -y
+
+# TODO: Trellis/Bedrock/Wordpress: https://www.youtube.com/watch?v=-pOKTtAfJ8M&ab_channel=WPCasts
 # TODO Ainsible Docker Swarm: https://imasters.com.br/devsecops/cluster-de-docker-swarm-com-ansible
 # TODO: REF: https://unix.stackexchange.com/questions/172179/gnome-shell-running-shell-script-after-session-starts
