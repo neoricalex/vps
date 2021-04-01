9f06e63c34450af4d89fa9097e9c0b1429c833df
neoricalex <neo.webmaster.2@gmail.com>
2021-04-01 15:03:31 -0300

    Atualização Automática via bash shell

--- compilar_nfdos.sh
+++ compilar_nfdos.sh
@@ -95,20 +95,18 @@ then
 
 	compilar_iso
 
-	vagrant plugin install vagrant-mutate
-	vagrant mutate $NFDOS_HOME/desktop/vagrant/$VERSAO_BOX_VAGRANT/NFDOS-$NFDOS_VERSAO.box libvirt
+	vagrant plugin install vagrant-libvirt
 
-
-	#echo "==> Adicionar a box neoricalex/nfdos ao Vagrant..."
-	#vagrant box add \
-	#	--name neoricalex/nfdos \
-	#	--provider $VERSAO_BOX_VAGRANT \
-	#	$NFDOS_HOME/desktop/vagrant/$VERSAO_BOX_VAGRANT/NFDOS-$NFDOS_VERSAO.box
+	echo "==> Adicionar a box neoricalex/nfdos ao Vagrant..."
+	vagrant box add \
+		--name neoricalex/nfdos \
+		--provider $VERSAO_BOX_VAGRANT \
+		$NFDOS_HOME/desktop/vagrant/$VERSAO_BOX_VAGRANT/NFDOS-$NFDOS_VERSAO.box
 
 	echo "==> Provisionando o NFDOS..."
 	vagrant box list
-    #vagrant up --provider $VERSAO_BOX_VAGRANT
-	#entrar_vps
+    vagrant up --provider $VERSAO_BOX_VAGRANT
+	entrar_vps
 
 elif vagrant status | grep "is running" > /dev/null;
 then
